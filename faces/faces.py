5d273886cb19a06c9da6d98683070e5ee8313d3a
naz-il-an <ilyas.n97@gmail.com>
2025-02-25 21:33:56 +0500

    first commit

---
+++ faces/faces.py
@@ -0,0 +1,11 @@
+def main():
+    text = input()
+    text = convert(text)
+    print(text)
+
+def convert(sentence):
+    sentence = sentence.replace(':)', '🙂')
+    sentence = sentence.replace(':(', '🙁')
+    return sentence
+   
+main()
