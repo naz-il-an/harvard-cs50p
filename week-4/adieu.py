11e2e75043466f69e31653b14f60634c6074a398
naz-il-an <ilyas.n97@gmail.com>
2025-03-15 21:25:56 +0500

    new file:   week-4/adieu.py

---
+++ week-4/adieu.py
@@ -0,0 +1,11 @@
+import inflect
+p = inflect.engine()
+names = []
+while True:
+    try:
+        names.append(input('Name: '))
+        
+    except EOFError:
+        break
+
+print('Adieu, adieu, to', p.join(names))
