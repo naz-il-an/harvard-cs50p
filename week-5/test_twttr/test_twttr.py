781e672cdf42624d752fae0641a7dd7f449845a1
naz-il-an <ilyas.n97@gmail.com>
2025-03-19 15:23:32 +0500

    modified:   week-5/test_twttr/test_twttr.py

--- week-5/test_twttr/test_twttr.py
+++ week-5/test_twttr/test_twttr.py
@@ -1,7 +1,20 @@
 from twttr import shorten
 
+def main():
+    test_shorten()
+    test_num()
+    test_punct()
 
-def shorten():
-    assert shorten('Twitter')
+def test_shorten():
+    assert shorten('twitter') == 'twttr'
+    assert shorten('TWITTER') == 'TWTTR'
+    assert shorten('TwItTeR') == 'TwtTR'
 
+def test_num():
+    assert shorten('1234') == '1234'
 
+def test_punct():
+    assert shorten('!?.,') == '!?.,'
+
+if __name__ == "__main__":
+    main()
